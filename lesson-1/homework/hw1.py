c8ecf77248b4244a20553e64f9d1ecbe4a601626
zokhidovjr <zabixullazohidov@gmail.com>
2025-11-21 21:28:44 +0500

    Add homework script for basic calculations

---
+++ lesson-1/homework/hw1.py
@@ -0,0 +1,29 @@
+#1
+a = int(input("a = "))
+P = 4 * a
+A = a ** 2
+print("Perimeter = ",P,"\nArea = ",A)
+
+#2
+import math 
+d = int(input("d = "))
+r = d/2
+C = math.pi * d 
+print("Radius = ",r,"\nLength = ", C)
+
+#3
+a = int(input("a = "))
+b = int(input("b = "))
+mean = (a+b)/2
+print("Mean = ",mean)
+
+#4
+a = int(input("a = "))
+b = int(input("b = "))
+sum = a+b
+product = a*b
+square_a = a**2
+square_b = b**2
+
+print("Sum =",sum,"\nProduct =",product)
+print("Square_a =",square_a,"\nSquare_b =",square_b)
